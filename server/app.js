ca72cc03c3c9d55a2b2da8f132b42efbd5287970
Seun Odewale <seunodewale.zone5@gmail.com>
2019-03-12 23:37:35 +0100

    restructure static files

--- server/app.js
+++ server/app.js
@@ -1,13 +1,26 @@
 import path from 'path';
+import http from 'http';
 import express from 'express';
+import socketIO from 'socket.io';
 
-const app = express();
 const publicPath = path.join(__dirname, './../public' );
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+
+server.listen(port, () => {
     console.log(`Server up and running on port ${port}`);
 });
