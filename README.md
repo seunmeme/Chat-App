a8254910c87bcf6b4553a0d66d11a2e87ee9bba6
Seun Odewale <seunodewale.zone5@gmail.com>
2019-03-14 00:16:43 +0100

    add form for messages and incoming messages

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-https://travis-ci.org/seunmeme/Chat-App.svg?branch=master
+[![Build Status](https://travis-ci.org/seunmeme/Chat-App.svg?branch=master)](https://travis-ci.org/seunmeme/Chat-App)
 # Chat-App
 A chat app built using node.js
--- public/js/index.js
+++ public/js/index.js
@@ -13,9 +13,24 @@ socket.on('connect', () => {
 
 socket.on('newMessage', (message) => {
     console.log('Message received', message);
+
+    let li = $('<li><li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
 });
 
 socket.on('disconnect', () => {
     console.log('Disconnected from the server');
 });
 
+$('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function() {
+
+    });
+})
