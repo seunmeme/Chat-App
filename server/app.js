3958aaa3492ab65ed45da5c2d686a22b6621aaa9
Seun Odewale <seunodewale.zone5@gmail.com>
2019-03-13 20:21:27 +0100

    add message broadcasting

--- server/app.js
+++ server/app.js
@@ -15,25 +15,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newEmail',  {
-        from: 'sol@example.com',
-        message: 'Hello there!',
-        createdAt: 6000
-    })
+    socket.on('createMessage', (message) => {
+        console.log('Message created', message);
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
 
     socket.emit('newMessage',  {
-        from: 'Seun',
-        message: 'Hey my people!',
-        createdAt: 6000
+        from: 'Admin',
+        message: 'Welcome onboard!',
+        createdAt: new Date().getTime()
     })
 
-    socket.on('createEmail', (newEmail) => {
-        console.log('Email created', newEmail);
-    });
-
-    socket.on('createMessage', (newMessage) => {
-        console.log('Message created', newMessage);
-    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined.',
+        createdAt: new Date().getTime()
+    })
+   
 
     socket.on('disconnect', () => {
         console.log('User was disconnected');
